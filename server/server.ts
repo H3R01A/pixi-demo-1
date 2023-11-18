879878e49c63734204c862c78e7943d1768cacca
H3R01A <aallen0001@gmail.com>
2023-11-18 10:24:19 -0800

    update server.ts to remove unnecessary comments and assignment to environment variables

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,17 @@
 import express from "express";
-const app = express();
 import { Request, Response } from "express";
 import path from "path";
 import cors from "cors";
 
-let port: number | string | undefined = process.env.PORT;
+const app = express();
+
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-if (port === undefined) {
-  port = 3000;
-}
 
-//console log to see path for - learning purposes
-// console.log(path.join(__dirname,'../public'));
 app.use(express.static(path.join(__dirname, '../dist')));
 
 
@@ -23,6 +19,6 @@ app.get("/", (req: Request, res: Response) => {
   return res.status(200).sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
